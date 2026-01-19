e5865390e40f707d58bf0789d711d072898bdffe
Dylan McDonald <dylan@deepdivedylan.io>
2026-01-19 14:44:49 -0800

    install i18n and tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import en from '../locales/en.json';
+import es from '../locales/es.json';
+type MessageSchema = typeof en;
+
+const browserLocale = navigator.languages[0].toLocaleLowerCase().includes('es') ? 'es' : 'en';
+const locale = window.localStorage.getItem('vueuse-i18n-locale') ?? browserLocale;
+const translator = createI18n<[MessageSchema], 'en' | 'es'>({
+    globalInjection: true,
+    legacy: false,
+    locale,
+    fallbackLocale: 'en',
+    messages: { en, es }
+});
+
+createApp(App).use(translator).mount('#app');
